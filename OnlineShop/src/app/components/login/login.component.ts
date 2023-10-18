2ca6e0feac949635b19fbecdf488fb5fd10e9f0c
JocaRistic <jocaristic5@gmail.com>
2023-10-19 01:26:13 +0200

    front final

---
+++ OnlineShop/src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { emailValidator, imePrezimeValidator } from 'src/app/validators/validators';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+
+  loginForma: FormGroup;
+
+  constructor(private fb: FormBuilder, private _authService: AuthService){}
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  //kreiranje forme pomocu FormBuilder
+  createLoginForm(){
+    this.loginForma = this.fb.group({
+      username: ['', Validators.compose([Validators.required, imePrezimeValidator])],
+      lozinka: ['', Validators.required]
+    })
+  }
+
+  //funckija sluzi za login korisnika, tako sto poziva funkciju login servisa authService
+  //i prosledjuje joj email i lozinku
+  ulogujSe(){
+    if(!this.loginForma.valid){
+      return;
+    }
+
+    const username = this.loginForma.controls['username'].value;
+    const lozinka = this.loginForma.controls['lozinka'].value;
+
+    this._authService.login(username, lozinka);
+
+    return false;
+  }
+
+}
